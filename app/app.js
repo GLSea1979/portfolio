53295ef7d3936d07d2667264b29e795b19ab920d
GLSea1979 <builtbygl@gmail.com>
2017-06-19 11:26:36 -0700

    build portfolio out a bit

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 8000);
 
 function nextSlide() {
+  console.log('inside bitches');
   slides[currentSlide].className = 'slide';
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].className = 'slide showing';
